3ac50d4ccab67b64109b6542b4035c1ec8ffb895
kartavya <kartavyas@gmail.com>
2023-12-08 15:16:29 +1100

    navigation comp added with link functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
+import RootLayout from "./pages/root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductPage /> },
+    ],
+  },
 ]);
 
 function App() {
